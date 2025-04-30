90601cee1def260e5a8231f111900b67f5e1cc96
Ebubekir Popczyk <ebubekir.popczyk.de@gmail.com>
2025-04-30 02:22:30 +0200

    implemented everything until chapter 10

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function Page() {
+async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
     <div>
       <p>Invoices Page</p>
